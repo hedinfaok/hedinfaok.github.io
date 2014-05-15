d910232189d02bd1f4b3ddf9452b31844a1da787
Rob Eden <reden@digitalpulp.com>
2014-05-15 00:51:51 -0400

    Heroku debugging ...

--- whatismyip/index.php
+++ whatismyip/index.php
@@ -1,15 +1,14 @@
 <?php
 
-$http_header = 'X-Forwarded-For';
-$PHPized_header = 'HTTP_X_FORWARDED_FOR';
+$PHPized_http_header = 'HTTP_X_FORWARDED_FOR';
 
 /**
  * Use REMOTE_ADDR. If there is a load balancer in front of us, use the last
  * value. See http://stackoverflow.com/a/18517550/3250348
  */
 $ipAddress = '';
-if (array_key_exists($http_header, $_SERVER)) {
-	$values = explode(',', $_SERVER[$PHPized_header]);
+if (array_key_exists($PHPized_http_header, $_SERVER)) {
+	$values = explode(',', $_SERVER[$PHPized_http_header]);
 	print_r($values);
     $ipAddress = array_pop($values);
 } else {
